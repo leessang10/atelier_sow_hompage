89d9aeb66d17845618c34061c7a2077fc7b2b734
leessang10 <leessang10@naver.com>
2025-04-24 00:26:33 +0900

    feat: aws amplify yaml 파일 추가

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,21 @@
-import {NextConfig} from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'i.ytimg.com'
-            },
-            {
-                hostname: 'zkmpkeilkhigsc2f.public.blob.vercel-storage.com'
-            },
-            {
-                hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com'
-            }
-        ]
-    }
-}
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i.ytimg.com',
+      },
+      {
+        hostname: 'zkmpkeilkhigsc2f.public.blob.vercel-storage.com',
+      },
+      {
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
